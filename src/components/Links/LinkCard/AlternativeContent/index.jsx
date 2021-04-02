1973b6545c4e56b0d8ec1766356ed35c9df58814
AntonioRdzNav <antoniordznav@outlook.com>
2021-04-01 20:01:22 -0600

    Implemented component containing an alternative card-content

---
+++ src/components/Links/LinkCard/AlternativeContent/index.jsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+import './index.scss';
+
+import CardActionButtons from '../ActionButtons';
+import CardTags from '../Tags';
+
+// NOTE: Content for small-sized cards
+const AlternativeCardContent = ({ name, url, tags, handleCopySnackbar }) => (
+  <>
+    <div className="Links-card-description">
+      <span className="Links-card-name truncate-text--single-line">{name}</span>
+      <span className="Links-card-url truncate-text--single-line">{url}</span>    
+    </div>
+    <CardTags tags={tags}/>
+    <CardActionButtons
+      url={url}
+      handleCopySnackbar={handleCopySnackbar}
+    />
+  </>
+);
+
+export default AlternativeCardContent;
