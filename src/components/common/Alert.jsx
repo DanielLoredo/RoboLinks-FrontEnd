9e623951b63445c147468d378fa6cab71ad5121e
AntonioRdzNav <antoniordznav@outlook.com>
2021-04-01 19:23:19 -0600

    Alert component that renders a customizable and reusable MUI/lab/Alert

---
+++ src/components/common/Alert.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import MuiAlert from '@material-ui/lab/Alert';
+
+const Alert = (props) => (
+  <MuiAlert elevation={6} variant="filled" {...props} />
+)
+
+export default Alert;
